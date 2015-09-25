90de678378ef18ccdcac170b320553e24b85fb2f
thaije <tj_474@hotmail.com>
2015-09-25 15:56:13 +0200

    met matplot, fock glumpy

--- test_matlab.py
+++ test_matlab.py
@@ -1,16 +1,19 @@
-#import matplotlib
-#matplotlib.use('GTKAgg') 
-#import time
-#import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('GTKAgg') 
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
 
-#plt.axis([0, 1000, 0, 1])
-#plt.ion()
-#plt.show()
-#
-#for i in range(1000):
-#	plt.cla()
-#	plt.axis([0, 1000, 0, 1])
-#	y = np.random.random()
-#	plt.scatter(i, y)
-#	plt.draw()
+plt.axis([0, 1000, 0, 1])
+plt.ion()
+plt.show()
+
+tic = time()
+for i in range(100):
+	plt.cla()
+	plt.axis([0, 1000, 0, 1])
+	y = np.random.random()
+	plt.scatter(i, y)
+	plt.draw()
+
+print "Matplotlib average FPS: %.2f" %(100/(time()-tic))
