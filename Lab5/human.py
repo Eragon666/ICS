25055338bbb1e0348628709273eb102d0baebe62
thaije <tj_474@hotmail.com>
2015-09-27 13:25:56 +0200

    infection etc.

--- Lab5/human.py
+++ Lab5/human.py
@@ -5,16 +5,13 @@ class human:
     # 0 = normaal = groen, 1 = infected = rood, 2 = immuun = blauw 
     colorList = ['green','red','blue']
 
-    def __init__(self, x, y, status, size):
+    def __init__(self, x, y, status):
         self.x = x
         self.y = y
         self.status = status
-        self.size = size
+        self.infectedOn = 0
+        self.fatalInfection = 0
 
     def getColor(self):
         return self.colorList[self.status]
 
-        #if (self.status == 1):
-        #    print 'No I am infected, I dont want Malaria :('
-        #else:
-        #    print 'wohoo I am save at '+ str(x) + ', ' + str(y) + ' !'
--- Lab5/main.py
+++ Lab5/main.py
@@ -35,7 +35,7 @@ class simulation():
         # Add a grid instance to the array
         for x in xrange(0, config['grid-y']):
             for y in xrange(0, config['grid-x']):
-                self.grid[x][y] = g.grid(x, y)
+                self.grid[x][y] = g.grid(x, y,config)
 
         self.addHumans()
         self.addMosquitos()
@@ -44,48 +44,66 @@ class simulation():
     def addHumans(self):
         """Add humans to the grid, only one human can be on each field"""
 
-        config = self.config
-        grid = self.grid
-
         # Get the numbers of humans in the initial state
-        popHuman = int(config['pop-human'])
+        popHuman = int(self.config['pop-human'])
 
         # Generate a list with randomly chosen infected humans based on
         # init-distr-human config value
-        infected = (np.random.rand(popHuman) > config['init-distr-human']).astype(int)
-        print infected
+        infected = (np.random.rand(popHuman) < self.config['init-distr-human']).astype(int)
+        #print infected
 
         for i in xrange(0, popHuman-1):
             found = False
 
             # Make sure that every grid place has only one human
-            while (not found):
-                (x,y) = self.getCoordinates()
+            (x,y) = self.findHomeHuman()
 
-                if (grid[x][y].checkFreedom() == True):
-                    found = True
+            self.grid[x][y].moveIn(h.human(x, y, infected[i]))
 
-            grid[x][y].moveIn(h.human(x, y, infected[i], config['human-size']))
-
-    def addMosquitos(self):
-        """ Add the mosquitos to the grid, there can be humans and mosquitos on
-        the same field and even multiple mosquitos on one field. """
-
-        config = self.config
-        grid = self.grid
-
-        # Get the numbers of mosquitos in the initial state
-        popMosq = int(config['pop-mosq'])
-
-        # Generate for each mosquito if it's infected or not and if it's hungry 
-        # in the initial state. Based on the initial distribution.
-        infected = (np.random.rand(popMosq) > config['init-distr-mosq']).astype(int)
-        hungry = (np.random.rand(popMosq) > config['init-hungry-mosq']).astype(int)
-
-        for i in xrange(1, popMosq-1):
+    def findHomeHuman(self):
+        """ Find a free cell for a human """
+        while (True):
             (x,y) = self.getCoordinates()
 
-            grid[x][y].flyIn(m.mosquito(x, y, self.t, infected[i], hungry[i], config['mosquito-size']))
+            if (self.grid[x][y].checkFreedom() == True):
+                return (x,y)
+
+
+    def cycleOfLife(self, x, y):
+        """ Deaths of humans by sickness and keeping the human population stable
+        by generating new humans if needed """
+
+        human = self.grid[x][y].human
+        # return if no (infected) human, nothing to do here
+        if human == None or human.status != 1:
+            return True
+
+
+        # if the human is infected, check if it dies based on death-rate and 
+        # how long he has the disease
+        if self.decision(human.infectedOn / float(self.config['death-delay'])):
+
+            # if he the infection was not fatal he becomes better, and possibly immune
+            if human.fatalInfection == 0:
+                human.infectedOn = 0
+                human.fatalInfection = 0
+                human.status = 0
+
+                if self.decision(self.config['immunity-change']):
+                    human.status = 2
+
+            # if the infection was fatal the human dies and a new baby is born
+            else:
+                human = None
+
+                # find a free cell, create a human and place it
+                (freeX,freeY) = self.findHomeHuman()
+                self.grid[x][y].moveIn(h.human(x, y, 0))
+
+        # if the human is not dead or better,  increase the amount of days it has the sickness
+        else: 
+            human.infectedOn += 1
+
 
     def getCoordinates(self):
         """ Generate a pair of coordinates, return it as a tuple"""
@@ -93,6 +111,10 @@ class simulation():
         y = random.randint(0, self.config['grid-y'] - 1)
 
         return (x, y)
+
+    def decision(self,probability):
+        """ Makes a decision based on a probability """
+        return np.random.random_sample() < probability
 
     def step(self):
         """ Do all the steps """
@@ -107,7 +129,9 @@ class simulation():
             start = time.time()
 
             for y in xrange(0, config['grid-x'] - 1):
+                # Mosquitos take a step, and check for human deaths
                 self.stepMosquitos(x, y)
+                self.cycleOfLife(x,y)
                 (status, objectSize) = self.grid[x][y].getInhabitants()
 
                 # add values to plot lists
@@ -117,17 +141,25 @@ class simulation():
                     self.plotType.append(status)
                     self.plotSize.append(objectSize)
 
+    def addMosquitos(self):
+        """ Add the mosquitos to the grid, there can be humans and mosquitos on
+        the same field and even multiple mosquitos on one field. """
 
+        # Get the numbers of mosquitos in the initial state
+        popMosq = int(self.config['pop-mosq'])
+
+        # Generate for each mosquito if it's infected or not and if it's hungry 
+        # in the initial state. Based on the initial distribution.
+        infected = (np.random.rand(popMosq) < self.config['init-distr-mosq']).astype(int)
+        hungry = (np.random.rand(popMosq) < self.config['init-hungry-mosq']).astype(int)
+
+        for i in xrange(1, popMosq-1):
+            (x,y) = self.getCoordinates()
+
+            self.grid[x][y].flyIn(m.mosquito(x, y, self.t, infected[i], hungry[i]))
                     
     def stepMosquitos(self, x, y):
         """ Calculate the step for the mosquitos """
-        #mosquitos = self.grid[x][y].getMosquitos()
-        #self.grid[x][y].clearMosquitos()
-
-        #for mosquito in mosquitos:
-        #    (x, y) = mosquito.step(config['grid-x'] - 1, config['grid-y'] - 1, self.t)
-        #    self.grid[x][y].flyIn(mosquito)
-
         # Loop through the list of mosquitos and remove the mosquito from the
         # list if checkMosquito returns False
         self.grid[x][y].mosquitos[:] = [ z for z in self.grid[x][y].mosquitos
@@ -136,8 +168,11 @@ class simulation():
 
     def checkMosquito(self, mosquito, current):
         """ Check if the mosquitos stays in the same place """
+        # check if the mosquito dies because of age
+        if self.decision(mosquito.age / float(self.config['mosq-max-age'])):
+            return False
 
-        # Do the calculations for the next position of the mosquito, and change hungry or not
+        # Do the calculations for the next position of the mosquito
         (x, y) = mosquito.step(config['grid-x'] - 1, config['grid-y'] - 1, self.t)
 
         # If the mosquito did not move, return True
@@ -146,8 +181,10 @@ class simulation():
 
         # Else place the mosquito in another cell and return False so the
         # mosquitos gets removed from the current field
+        # The mosquito also eats a human if there is one in its new cell
         self.grid[x][y].flyIn(mosquito)
         return False
+
 
 
 if __name__ == '__main__':
@@ -165,7 +202,7 @@ if __name__ == '__main__':
 
     startSteps = time.time()
 
-    for i in xrange(10):
+    for i in xrange(100):
         startSteps = time.time()
         sim.step()
         endSteps = time.time()
